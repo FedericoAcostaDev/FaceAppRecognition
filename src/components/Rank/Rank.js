f832ebd8c8650ecf4be64b94079554932a4a7e30
FedericoAcostaDev <fedeacos045@gmail.com>
2021-06-29 10:45:30 -0300

    Problem with particles

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+//pure function
+const Rank = () => {
+  return (
+    <div>
+    <div className="white f3">
+        {'Federico, Your current rank is ...'}
+
+        </div>
+    <div className="white f1">
+    {'#5'}
+
+    </div>
+    </div>
+  );
+};
+
+export default Rank;
