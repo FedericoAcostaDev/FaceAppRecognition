e61e4f46bdd1cab18e24b2c3c36c3b0d026858f5
FedericoAcostaDev <fedeacos045@gmail.com>
2021-07-06 18:21:32 -0300

    SignIn function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FaceRecognition from "./components/FaceRecognition/FaceRecognition.js";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js";
 import Rank from "./components/Rank/Rank.js";
+import SingIn from "./components/SingIn/SingIn.js";
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
 import "./App.css";
@@ -31,57 +32,71 @@ class App extends Component {
       input: "",
       imageUrl: "",
       box: {},
+      route: "SignIn",
     };
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
-    return{
+    return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   displayFaceBox = (box) => {
     console.log(box);
-    this.setState({box: box});
-  }
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
   };
-  
+
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
-    .predict(
-      Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-    .catch(err => console.log(err));
-      }
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((err) => console.log(err));
+  };
 
-
+  onRouteChange = () => {
+    this.setState({route:'home'})
+  }
   render() {
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
         <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        {this.state.route === "SignIn" ? 
+          <SingIn onRouteChange={this.onRouteChange}/>
+         : 
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        }
       </div>
     );
   }
+  
 }
 
 export default App;
---
+++ src/components/SingIn/SingIn.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+//pure function
+const SingIn = ({ onRouteChange }) => {
+  return (
+    //tachyons SignIn
+    <article className="br3 ba  b--black-10 mv4 w-100 w-50-m w-25-l mw5 shadow-5 center">
+      <article className="pa4 black-80">
+        <form action="sign-up_submit" method="get" accept-charset="utf-8">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="ph0 mh0 fw6 clip">Sign Up</legend>
+            <div className="mt3">
+              <label className="db fw4 lh-copy f6" for="email-address">
+                Email address
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent w-100 measure"
+                type="email"
+                name="email-address"
+                id="email-address"
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw4 lh-copy f6" for="password">
+                Password
+              </label>
+              <input
+                className="b pa2 input-reset ba bg-transparent"
+                type="password"
+                name="password"
+                id="password"
+              />
+            </div>
+          </fieldset>
+          <div className="mt3">
+            <input
+              onClick={onRouteChange}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6"
+              type="submit"
+              value="Sign In"
+            />
+          </div>
+          <div className="1h-copy mt3">
+            <a href="#0" className="f6 link dim black db">
+              Register
+            </a>
+          </div>
+        </form>
+      </article>
+    </article>
+  );
+};
+
+export default SingIn;
