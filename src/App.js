8a01ea1fba4d5a3fea14d6d9c3df5184dbc9990b
FedericoAcostaDev <fedeacos045@gmail.com>
2021-08-10 17:40:15 -0300

    nav buttons fixed

--- src/App.js
+++ src/App.js
@@ -7,13 +7,8 @@ import Rank from "./components/Rank/Rank.js";
 import SignIn from "./components/SignIn/SignIn.js";
 import Register from "./components/Register/Register.js";
 import Particles from "react-particles-js";
-import Clarifai from "clarifai";
+
 import "./App.css";
-
-
-const app = new Clarifai.App({
-  apiKey: "1a0efca46056437bbbeeb505fe64c696",
-});
 
 const particlesOptions = {
   particles: {
@@ -25,7 +20,7 @@ const particlesOptions = {
       },
     },
   },
-}; 
+};
 const initialState = {
   input: "",
   imageUrl: "",
@@ -37,24 +32,26 @@ const initialState = {
     name: "",
     email: "",
     entries: 0,
-    joined: ""
-  }
-}
+    joined: "",
+  },
+};
 class App extends Component {
   constructor() {
     super();
-    this.state = initialState
+    this.state = initialState;
   }
 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
+    this.setState({
+      user: {
+        id: data.id,
         name: data.name,
         email: data.email,
         entries: data.entries,
-        joined: data.joined
-    }})
-  }
+        joined: data.joined,
+      },
+    });
+  };
 
   //the app works on port 3001..dont ask me why
 
@@ -83,31 +80,36 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL,
-         this.state.input)
+    fetch("http://localhost:3000/imageurl", {
+      method: "post",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+    .then (response => response.json())
       .then((response) => {
         if (response) {
-          fetch('http://localhost:3000/image', {
-            method:'put',
-            headers: {'content-type':'application/json'},
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "content-type": "application/json" },
             body: JSON.stringify({
-              id: this.state.user.id
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
-          })
-          .then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, {entries: count}))
-          })
-          .catch(console.log)
+            .catch(console.log);
         }
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
       .catch((err) => console.log(err));
   };
 
   onRouteChange = (route) => {
-    if (route === "SignOut") {
+    if (route === "signout") {
       this.setState(initialState);
     } else if (route === "home") {
       this.setState({ isSignedIn: true });
@@ -126,7 +128,10 @@ class App extends Component {
         {route === "home" ? (
           <div>
             <Logo />
-            <Rank name={this.state.user.name} entries={this.state.user.entries}/> 
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -134,9 +139,12 @@ class App extends Component {
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
         ) : route === "signin" ? (
-          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} /> 
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
-          <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
         )}
       </div>
     );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,13 +6,13 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
             return (
 
         <nav style={{display: 'flex',justifyContent: 'flex-end'}} >
-            <p onClick={() => onRouteChange('SignOut')} className= 'f3 link dim black underline pa3 pointer'>Sign Out</p>
+            <p onClick={() => onRouteChange('signout')} className= 'f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
             );
         } else {
             return (
             <nav style={{display: 'flex',justifyContent: 'flex-end'}} >
-            <p onClick={() => onRouteChange('SignIn')} className= 'f3 link dim black underline pa3 pointer'>Sign In</p>
+            <p onClick={() => onRouteChange('signin')} className= 'f3 link dim black underline pa3 pointer'>Sign In</p>
             <p onClick={() => onRouteChange('Register')} className= 'f3 link dim black underline pa3 pointer'>Register</p>
         </nav>
             );
